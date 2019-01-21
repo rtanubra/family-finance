a3d7be98d51fef3ef1a265d4f41933e2b4aa3a4c
rtanubra <rdtanubrata@gmail.com>
2019-01-21 15:29:43 -0500

    Working on building views and templates

--- mysite/finance/views.py
+++ mysite/finance/views.py
@@ -1,6 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
+from .models import Group, People, Category, LineItem
 # Create your views here.
 def index(request):
+    return HttpResponse("Hello, world. You are at the polls index.")
+
+def person(request):
+    return HttpResponse("Hello, world. You are at the polls index.")
+
+def person_summary(request,person,month_year):
+    return HttpResponse("Hello, world. You are at the polls index.")
+
+def purchase(request):
     return HttpResponse("Hello, world. You are at the polls index.")
